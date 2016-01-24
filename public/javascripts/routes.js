bc836c32a75e11ca7b3021b501d7ed5bf2c69c7b
Kelly Byrne <kellyannbyrne@utexas.edu>
2016-01-24 16:22:06 -0600

    close to complete

---
+++ public/javascripts/routes.js
@@ -0,0 +1,8 @@
+app.config(function($routeProvider, $locationProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: '/partials/home.html',
+			controller: 'HomeCtrl'
+		})
+		$locationProvider.html5Mode(true)
+});
